0f2033cf20bdfee95559a79e795543d800b4a3ae
jonas-andrade <jonas6116m@outlook.com>
2022-06-20 09:56:57 -0300

    HelloWorld: Concatenando Strings com array de Char[]

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,16 @@
 public class HelloWorld{
     public static void main(String []arguments) {
         System.out.println("Hello World");
+         char caracteres[] =  {'L','4','r','4','n','J','4'};
 
+        var str_laranja =  new String(caracteres);
+        var str_amarelo =  "4m4r3l0";
+
+        
+        // concatenando strings...
+        System.out.println("Array de caracteres: "+str_laranja+"\nTipo primitivo (class) String: "+str_amarelo);
+
+        
        
        
     }
