91d9a70ae126ffddd5fd136f7660ecee21df5737
nome de usuário <ned.dennis05@outloook.com>
2022-06-23 18:32:00 -0300

    test of commit

---
+++ conversor.py
@@ -0,0 +1,12 @@
+# z = inpult("") =entrada de dados # print(z) = exibir dados
+
+#a = input("digite o número binário: ") # print(a)
+
+binario = int(input("digite o numero binário:"))
+resto = hex(binario%16)
+print(resto)
+
+#n = int(input(":"))
+#conversao = hex(n)
+#print(conversao)
+
