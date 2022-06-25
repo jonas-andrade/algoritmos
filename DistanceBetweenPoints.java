8e84ef279b5da0156cecd0f128a04253271df46e
jonas-andrade <jonas6116m@outlook.com>
2022-06-25 17:30:26 -0300

    dAB² = (xB – xA)² + (yB – yA)²: algoritimo melhorado

--- DistanceBetweenPoints.java
+++ DistanceBetweenPoints.java
@@ -3,22 +3,22 @@ import java.util.Locale;
 // calculo da distancia entre dois pontos:  dAB² = (xB – xA)² + (yB – yA)².
 
   class Ponto{
-    int x1;int x2;
-    int y1;int y2;
-    public Ponto(int x1,int x2,int y1,int y2){
-    this.x1 = x1;this.x2 = x2;this.y1 = y1;this.y2 = y2;} 
+    int x;int y;
+    
+    public Ponto(int x,int y){this.x = x;this.y = y;} 
 
 }
 
 public class DistanceBetweenPoints {
-    public static double calcEuclides(Ponto p){
-        return Math.sqrt((Math.pow((p.x2 -p.x1), 2) + Math.pow((p.y2-p.y1), 2)));
+    public static double calcEuclides(Ponto p1, Ponto p2){
+        return Math.sqrt((Math.pow((p2.x-p1.x), 2) + Math.pow((p2.y-p1.y), 2)));
     }
 
     public static void main(String[] args) {
-        Ponto p = new Ponto(1,3,4,6);
+        Ponto p1 = new Ponto(-3, -11);
+        Ponto p2 = new Ponto(2,1);
         
-        System.out.printf(Locale.US,"%.2f",calcEuclides(p));
+        System.out.printf(Locale.US,"%.2f",calcEuclides(p1,p2));
 
         
     }
