c88b83e8388ae660877194bd07789b5895c2f707
jonas-andrade <jonas6116m@outlook.com>
2022-06-28 20:16:42 -0300

    step completed: hex challenge

--- Conversor.java
+++ Conversor.java
@@ -4,8 +4,12 @@ class Conversor{
         // DecimalToBinary dtb = new DecimalToBinary();
         // dtb.inputScanner();dtb.calc(); dtb.result();
 
-        BinaryToDecimal btd = new BinaryToDecimal();
-        btd.inputScanner();btd.result();
+        // BinaryToDecimal btd = new BinaryToDecimal();
+        // btd.inputScanner();btd.result();
+
+        DecimalToHex dth = new DecimalToHex();
+        dth.inputScanner();dth.calcHex();dth.viewHex();
+       
     }
  
 }
