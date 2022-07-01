85134e9ae2cf308d6f6a09e20560425519adab1b
jonas-andrade <jonas6116m@outlook.com>
2022-07-01 19:51:28 -0300

    HexToDecimal: 100%% finished

--- HexToDecimal.java
+++ HexToDecimal.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class HexToDecimal {
     int count;
     String in;
-    char []hex ={'j','j','j','j','j','j','j','j',};
+    char []hex = {'j','j','j','j','j','j','j','j'};
     int base = 16;
     int decimal = 0;
 
@@ -18,20 +18,20 @@ public class HexToDecimal {
         } catch (Exception e) {System.err.print(e);}
     }
     public int calcHex(){
-         
-        for(int i=this.in.length()-1;i>=0;i--){//indice
+        for(int i=this.in.length()-1;i>=0;i--){
             hex[i] = this.in.toCharArray()[i];
-            int number = (hex[i] == 'A'|| hex[i] == 'a')? 10 :(hex[i] == 'B'||hex[i] == 'b') ? 11: (hex[i] == 'C'||hex[i] == 'c')? 12:(hex[i] == 'D'||hex[i] == 'd')? 13:(hex[i] == 'E'||hex[i] == 'e')? 14 : (hex[i] == 'F'||hex[i] == 'f')? 15: 16;
+            int number = (hex[i] == 'A'|| hex[i] == 'a')? 10 : (hex[i] == 'B'||hex[i] == 'b') ? 11:
+            (hex[i] == 'C'||hex[i] == 'c')? 12: (hex[i] == 'D'||hex[i] == 'd')? 13:
+            (hex[i] == 'E'||hex[i] == 'e')? 14: (hex[i] == 'F'||hex[i] == 'f')? 15: Character.getNumericValue(hex[i]);
             count += (number > 9 && number <= 16 )?
-            count += number*Math.pow(base, (this.in.length()-i-1)): 
+             number*Math.pow(base, (this.in.length()-i-1)): 
             Character.getNumericValue(hex[i])*Math.pow(base, (this.in.length()-i-1));      
-    }
-   
-
+       }
     return count;
     }
     public void viewDecimal(){
-        System.out.println("Resultado em decimal: "+calcHex());count = 0;
+        System.out.println("Resultado em decimal: "+calcHex());
+        count = 0;
    
     }
 
